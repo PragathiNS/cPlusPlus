82e857da12115af6117ce6bced0642b62bd2c367
Pragathi N <pragathi.code@gmail.com>
2017-04-20 21:54:42 -0500

    Input and Output

---
+++ io.cpp
@@ -0,0 +1,23 @@
+/*
+Author: Pragathi N
+Date: 4/20/2017
+Objective: In this challenge, we're practicing reading input from stdin and printing output to stdout.
+Task: Read 3 numbers from stdin and print their sum to stdout.
+*/
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c << endl;
+    return 0;
+}
